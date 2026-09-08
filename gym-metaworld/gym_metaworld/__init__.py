11fabd781eb63d2ca589313ea4d41620f3df8a61
ssskeyi <ycb0321@outlook.com>
2026-09-08 11:25:22 +0800

    clean

--- gym-metaworld/gym_metaworld/__init__.py
+++
@@ -1,7 +0,0 @@
-from gymnasium.envs.registration import register
-
-register(
-    id="gym_metaworld/metaworld-v0",
-    entry_point="gym_metaworld.env:MetaWorldVitaEnv",
-    nondeterministic=True,
-)
